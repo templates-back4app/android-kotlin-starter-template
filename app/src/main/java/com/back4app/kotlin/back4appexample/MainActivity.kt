de279aad89c61bd28446ca57a506d575fed1f357
Cihan Gül <cihangulx@gmail.com>
2020-12-04 20:05:11 +0300

    Save example.

--- app/src/main/java/com/back4app/kotlin/back4appexample/MainActivity.kt
+++ app/src/main/java/com/back4app/kotlin/back4appexample/MainActivity.kt
@@ -2,7 +2,10 @@ package com.back4app.kotlin.back4appexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
 import com.parse.Parse
+import com.parse.ParseObject
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,5 +18,18 @@ class MainActivity : AppCompatActivity() {
             .clientKey(getString(R.string.parse_client_key))
             .server(getString(R.string.parse_server_url))
             .build());
+
+        val textView = findViewById<TextView>(R.id.textView)
+
+        val firstObject = ParseObject("FirstClass")
+        firstObject.put("message","Hey ! First message from android. Parse is now connected")
+        firstObject.saveInBackground {
+            if (it != null){
+                it.localizedMessage?.let { message -> Log.e("MainActivity", message) }
+            }else{
+                Log.d("MainActivity","Object saved.")
+                textView.text = String.format("Object saved. %s", firstObject.objectId)
+            }
+        }
     }
 }
